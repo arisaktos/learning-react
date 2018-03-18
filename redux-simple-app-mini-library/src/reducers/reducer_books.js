e79991a8af8884e8a9f0cca27460f69242df08c5
Alicja <arisusaktos@gmail.com>
2018-03-18 19:51:26 +0100

    mini library app

---
+++ redux-simple-app-mini-library/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return [
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 39},
+        {title: 'Lord of the Rings', pages: 85},
+        {title: 'The dark tower', pages: 1}
+    ]
+}
+
